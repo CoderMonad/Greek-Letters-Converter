bc58ce327bc736b7416a2483931a26828be83b5b
GamerMan7799 <GamerMan7799@users.noreply.github.com>
2019-03-29 14:47:26 -0400

    Update Perl to v0.9.1

--- WIP/Perl/Greekletters.pl
+++ WIP/Perl/Greekletters.pl
@@ -52,7 +52,7 @@ Sequential Number arrays
 
 use POSIX;
 
-$Version_Number = "v0.9.0";
+$Version_Number = "v0.9.1";
 $datestring = localtime();
 
 sub GreekLetter {
@@ -61,29 +61,29 @@ sub GreekLetter {
 	foreach $item (@_) {
 		if ($item == 0) {print " ";}
 		elsif ($item == 1) {print "Alpha ";}
-		elsif ($item == 1) {print "Beta ";}
-		elsif ($item == 1) {print "Gamma ";}
-		elsif ($item == 1) {print "Delta ";}
-		elsif ($item == 1) {print "Epsilon ";}
-		elsif ($item == 1) {print "Zeta ";}
-		elsif ($item == 1) {print "Eta ";}
-		elsif ($item == 1) {print "Theta ";}
-		elsif ($item == 1) {print "Iota ";}
-		elsif ($item == 1) {print "Kappa ";}
-		elsif ($item == 1) {print "Lambda ";}
-		elsif ($item == 1) {print "Mu ";}
-		elsif ($item == 1) {print "Nu ";}
-		elsif ($item == 1) {print "Xi ";}
-		elsif ($item == 1) {print "Omicron ";}
-		elsif ($item == 1) {print "Pi ";}
-		elsif ($item == 1) {print "Rho ";}
-		elsif ($item == 1) {print "Sigma ";}
-		elsif ($item == 1) {print "Tau ";}
-		elsif ($item == 1) {print "Upsilon ";}
-		elsif ($item == 1) {print "Phi ";}
-		elsif ($item == 1) {print "Chi ";}
-		elsif ($item == 1) {print "Psi ";}
-		elsif ($item == 1) {print "Omega ";}
+		elsif ($item == 2) {print "Beta ";}
+		elsif ($item == 3) {print "Gamma ";}
+		elsif ($item == 4) {print "Delta ";}
+		elsif ($item == 5) {print "Epsilon ";}
+		elsif ($item == 6) {print "Zeta ";}
+		elsif ($item == 7) {print "Eta ";}
+		elsif ($item == 8) {print "Theta ";}
+		elsif ($item == 9) {print "Iota ";}
+		elsif ($item == 10) {print "Kappa ";}
+		elsif ($item == 11) {print "Lambda ";}
+		elsif ($item == 12) {print "Mu ";}
+		elsif ($item == 13) {print "Nu ";}
+		elsif ($item == 14) {print "Xi ";}
+		elsif ($item == 15) {print "Omicron ";}
+		elsif ($item == 16) {print "Pi ";}
+		elsif ($item == 17) {print "Rho ";}
+		elsif ($item == 18) {print "Sigma ";}
+		elsif ($item == 19) {print "Tau ";}
+		elsif ($item == 20) {print "Upsilon ";}
+		elsif ($item == 21) {print "Phi ";}
+		elsif ($item == 22) {print "Chi ";}
+		elsif ($item == 23) {print "Psi ";}
+		elsif ($item == 24) {print "Omega ";}
 		else {print "ERROR ";}
 	}
 }
@@ -97,32 +97,32 @@ sub main {
 	$num5 = 0;
 	
 	do {
-		print "Please enter a number between 0 and 8,308,824: ";
+		print ("Please enter a number between 0 and 8,308,824: ");
 		my $input = <STDIN>;
-	} while ( $input > 8308824 || $input < 0)
+	} while ( $input > 8308824 || $input < 0);
 	
 	if ( $input > 24 ) {
 		$num2 = floor( ($input - 1) / 24);
 		$num1 = $input - $num2 * 24;
-	} 
-	else {
+	} else {
 		$num1 = floor($input);
 	}
 	
 	if ( $num2 > 24 ) {
-		$num3 = floor(($num2 - 1) / 24)
-		$num2 = $num2 - ($num3 * 24)
+		$num3 = floor(($num2 - 1) / 24);
+		$num2 = $num2 - ($num3 * 24);
 	}
 	
 	if ( $num3 > 24 ) {
-		$num4 = floor(($num3 - 1) / 24)
-		$num3 = $num3 - ($num4 * 24)
+		$num4 = floor(($num3 - 1) / 24);
+		$num3 = $num3 - ($num4 * 24);
 	} 
 	
 	if ( $num4 > 24 ) {
-		$num5 = floor(($num4 - 1) / 24)
-		$num2 = $num4 - ($num5 * 24)
+		$num5 = floor(($num4 - 1) / 24);
+		$num2 = $num4 - ($num5 * 24);
 	}
+    
 	
 	print "\n";
 	
@@ -131,4 +131,3 @@ sub main {
 
 
 main();
-
